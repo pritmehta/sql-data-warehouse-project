3e611b9a0951bccbcd3dd7816fe340d47dbb9105
Prit Mehta <156615295+pritmehta@users.noreply.github.com>
2026-01-24 01:49:34 +0530

    Update init_database.sql
    added warning message

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -6,6 +6,10 @@ Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this scriot will drop the entire 'DataWarehouse' database if it exists.
+    All data in the database will be permanently deleted. Ensure to have proper backups before running the script.
 */
 
 USE master;
